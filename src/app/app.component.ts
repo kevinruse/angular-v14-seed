4138798afd52b23cbc1adace7f872bd1135d5d91
Kevin Ruse <kevin@kevinruse.com>
2022-07-06 10:29:48 -0700

    challenge exercise 1 build a component for the seed application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app',
     template: `<div>
+        <app-header></app-header>
         <p>The App is working!</p>
     </div>`
 })
