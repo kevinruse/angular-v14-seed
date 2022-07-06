a771d0fb4660e7125a35844693feee57c401bedf
Kevin Ruse <kevin@kevinruse.com>
2022-07-06 10:32:51 -0700

    challenge exercise 2 using the templateUrl property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app',
-    template: `<div>
-        <app-header></app-header>
-        <p>The App is working!</p>
-    </div>`
+    templateUrl: './app.component.html'
 })
 export class AppComponent {}
