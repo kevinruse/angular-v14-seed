67c2967c86127cc7745d45ad40da9f38eb909a98
Kevin Ruse <kevin@kevinruse.com>
2022-07-06 10:22:24 -0700

    guided exercise 9 create the app component

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app',
+    template: `<div>
+        <p>The App is working!</p>
+    </div>`
+})
+export class AppComponent {}
