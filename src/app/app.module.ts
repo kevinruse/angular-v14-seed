595640d70852be0588ac9dd7d102d6683989f276
Kevin Ruse <kevin@kevinruse.com>
2022-07-07 11:59:50 -0700

    guided exercise 10 using 3rd party libraries and advanced styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { ChildComponent } from './child/child.component';
+import { HeaderComponent } from "./header/header.component";
+
+import { FormInputComponent } from './form-input/form-input.component';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
-     imports: [ BrowserModule ],
-     declarations: [ AppComponent, HeaderComponent, ChildComponent ],
+     imports: [ BrowserModule, FontAwesomeModule ],
+     declarations: [ AppComponent, 
+                     ChildComponent, 
+                     FormInputComponent,
+                     HeaderComponent ],
      bootstrap: [ AppComponent ]
 })
 
