9b4497677e4939da3ef4e3db8d0bee32b7fa2c47
Kevin Ruse <kevin@kevinruse.com>
2022-07-07 09:56:04 -0700

    guided exercise 9 styling with the css cascade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
+import { ViewEncapsulation } from "@angular/core";
 import { Component } from "@angular/core";
 
 @Component({
     selector: 'app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {}
