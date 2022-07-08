9cb4594d152ca8ac166b488e32bc6cd230808c68
Kevin Ruse <kevin@kevinruse.com>
2022-07-08 09:20:45 -0700

    guided exercise 22 understanding @ViewChild decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
-import { ViewEncapsulation } from '@angular/core';
+import {
+  ViewChild,
+  ViewChildren,
+  ViewEncapsulation,
+  QueryList,
+  ElementRef,
+} from '@angular/core';
 import { Component } from '@angular/core';
+import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app',
@@ -8,6 +15,28 @@ import { Component } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
+  @ViewChild(ChildComponent, { static: false }) childView!: ChildComponent;
+  @ViewChildren(ChildComponent) childrenView!: QueryList<any>;
+  @ViewChild('fruit', { static: false }) fruit!: ElementRef;
+
+  /*   constructor() {
+    console.log(this.childView);
+  } */
+
+  ngAfterViewInit(): void {
+    console.log(this.childrenView);
+    const children: ChildComponent[] = this.childrenView.toArray();
+    console.log(children);
+    console.log(this.fruit);
+    console.table(this.childView);
+  }
+
+  addFruit(): void {
+    this.childView.fruitCounter();
+    this.fruit.nativeElement.innerText = this.childView.fruitStatus;
+    console.log(this.childView.fruitStatus);
+  }
+
   loggedIn!: boolean;
 
   logIn(evt: boolean): void {
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -6,12 +6,18 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./child.component.css'],
 })
 export class ChildComponent implements OnInit {
+  fruitStatus: number = 0;
+
   @Output()
   login = new EventEmitter<boolean>();
 
   loginOutput(login: boolean): void {
     this.login.emit(login);
     console.log(login);
+  }
+
+  fruitCounter(): number {
+    return this.fruitStatus++;
   }
 
   constructor() {}
